f1570e522b2e93c7ad489a1fce00877ad55b9883
lakshmipriya-codes <lakshmipriyavempalli21@gmail.com>
2026-06-13 18:45:00 +0530

    api doc added

--- 10-frontend.sh
+++ 10-frontend.sh
@@ -7,23 +7,25 @@ check_root
 dnf module disable nginx -y &>>$LOGS_FILE
 dnf module enable nginx:1.24 -y &>>$LOGS_FILE
 dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Installing nginx"
+validate $? "Installing nginx"
 
 rm -rf /usr/share/nginx/html/*  &>>$LOGS_FILE
-VALIDATE $? "Removed default code"
+validate $? "Removed default code"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOGS_FILE
-VALIDATE $? "Downloaded and extracted frontend code"
+validate $? "Downloaded and extracted frontend code"
 
 rm -rf /etc/nginx/nginx.conf
-VALIDATE $? "Removed nginx conf"
+validate $? "Removed nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copied roboshop nginx conf"
+validate $? "Copied roboshop nginx conf"
 
 
+systemctl restart nginx
 systemctl enable nginx &>> $LOGS_FILE
-app_restart
+validate $? "Enabled and restarted nginx"
+
 print_total_time
